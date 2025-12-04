a2a6d7c69861eacada82827ae772aaf0a51348b0
pedroorozco <122556028+pedroorozco@users.noreply.github.com>
2025-12-03 20:57:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Collapsable hamburger menu
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('#nav-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    });
+});
